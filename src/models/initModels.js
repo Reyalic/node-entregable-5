718871c569592e1a277f0a8c4e3b3f46a60d0259
Reyalic <martinvt360@gmail.com>
2022-10-22 18:34:55 -0500

    models

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,7 +1,13 @@
 const Users = require("./users.models")
+const Conversations = require("./conversations.model")
+const Participants = require("./participants.model")
+const Message = require("./message.model")
+
 
 const initModels = () => {
-    Users
+    Users.hasMany(Conversations)
+
+    Conversations.belongsTo(Users)
 }
 
 module.exports = initModels
